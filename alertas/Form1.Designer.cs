67862d8c4d9a758375a39f81f4acd9f31a0740b3
VICTOR DA SILVA OLIVEIRA <victor.soliveira21@senacsp.edu.br>
2025-08-19 11:37:33 -0300

    19/08

---
+++ alertas/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace alertas
+{
+    partial class Alertas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAlertaSimples = new System.Windows.Forms.Button();
+            this.btnAlertaRobusto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(282, 38);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(202, 24);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Teste os alertas abaixo";
+            // 
+            // btnAlertaSimples
+            // 
+            this.btnAlertaSimples.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(103)))), ((int)(((byte)(86)))));
+            this.btnAlertaSimples.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlertaSimples.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAlertaSimples.Location = new System.Drawing.Point(300, 111);
+            this.btnAlertaSimples.Name = "btnAlertaSimples";
+            this.btnAlertaSimples.Size = new System.Drawing.Size(200, 57);
+            this.btnAlertaSimples.TabIndex = 1;
+            this.btnAlertaSimples.Text = "Alerta Simples";
+            this.btnAlertaSimples.UseVisualStyleBackColor = false;
+            this.btnAlertaSimples.Click += new System.EventHandler(this.btnAlertaSimples_Click);
+            // 
+            // btnAlertaRobusto
+            // 
+            this.btnAlertaRobusto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAlertaRobusto.FlatAppearance.BorderSize = 0;
+            this.btnAlertaRobusto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnAlertaRobusto.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlertaRobusto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnAlertaRobusto.Location = new System.Drawing.Point(300, 200);
+            this.btnAlertaRobusto.Name = "btnAlertaRobusto";
+            this.btnAlertaRobusto.Size = new System.Drawing.Size(200, 49);
+            this.btnAlertaRobusto.TabIndex = 2;
+            this.btnAlertaRobusto.Text = "Alerta Robusto";
+            this.btnAlertaRobusto.UseVisualStyleBackColor = false;
+            this.btnAlertaRobusto.Click += new System.EventHandler(this.btnAlertaRobusto_Click);
+            // 
+            // Alertas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAlertaRobusto);
+            this.Controls.Add(this.btnAlertaSimples);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Alertas";
+            this.Text = "Alertas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnAlertaSimples;
+        private System.Windows.Forms.Button btnAlertaRobusto;
+    }
+}
+
---
+++ alertas/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alertas
+{
+    public partial class Alertas : Form
+    {
+        public Alertas()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlertaSimples_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Essa é uma mensagem simple", "Com um titulo");
+        }
+
+        private void btnAlertaRobusto_Click(object sender, EventArgs e)
+        {
+            DialogResult resposta = MessageBox.Show("Vamos programar um sistema",           
+                                                        "Titulo aqui",                 
+                                                         MessageBoxButtons.YesNo,   
+                                                         MessageBoxIcon.Question);
+
+            if(resposta == DialogResult.Yes)
+            {
+                MessageBox.Show("Respondeu que sim");
+            } else if (resposta == DialogResult.No)
+            {
+                MessageBox.Show("Respondeu que não");
+            }
+        }   
+    }
+}
